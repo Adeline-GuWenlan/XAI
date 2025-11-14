9dfb1d587d2147d48156c82637c927c3a873e324
Adeline <wg2381@nyu.edu>
2025-11-14 19:51:59 +0800

    Just renamed a folder. will set the new models taking pauli as input here

--- INSPECT/command.sh
+++ INSPECT/command.sh
@@ -1,2 +1,9 @@
 python '/Users/guwenlan/Desktop/INSPECT/DataGeneration/RoM_label/RoM_poisson.py' --num 200 --qubits 2 --lam 1.5 --output '/Users/guwenlan/Desktop/INSPECT/Data' --seed 114514
 python '/Users/guwenlan/Desktop/INSPECT/DataGeneration/RoM_label/RoM_poisson.py' --num 1000 --qubits 2 --lam 31 --output '/Users/guwenlan/Desktop/INSPECT/Data' --seed 114514
+python '/Users/guwenlan/Desktop/XAI/INSPECT/DataGeneration/Expectation/generate_complete_dataset.py' -n 2 --num 50000 -o "/Users/guwenlan/Desktop/XAI/Full_Ori"
+
+python '/Users/guwenlan/Desktop/XAI/INSPECT/DataGeneration/Expectation/generate_complete_dataset.py' -n 2 --num 100000 -o /Users/guwenlan/Desktop/XAI/Full_Ori/2q
+
+python '/Users/guwenlan/Desktop/XAI/INSPECT/DataGeneration/Expectation/generate_complete_dataset.py' -n 3 --num 100000 -o /Users/guwenlan/Desktop/XAI/Full_Ori/3q
+python '/Users/guwenlan/Desktop/XAI/INSPECT/DataGeneration/Expectation/generate_complete_dataset.py' -n 4 --num 150000 -o /Users/guwenlan/Desktop/XAI/Full_Ori/4q
+python '/Users/guwenlan/Desktop/XAI/INSPECT/DataGeneration/Expectation/generate_complete_dataset.py' -n 5 --num 200000 -o /Users/guwenlan/Desktop/XAI/Full_Ori/5q
